b51dbd7d7d342fe2de92c359f14143e9b5829997
davidmrtn07 <davidmrtn07@gmail.com>
2024-05-03 11:21:42 -0500

    bonus inital save

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public static class DataAccessFactory
+    {
+        public static IDataAccess GetDataAccessType(string databaseType)
+        {
+            switch (databaseType.ToLower())
+            {
+                case "mongo":
+                    return new MongoDataAccess();
+                case "sq1":
+                    return new SQLDataAccess();
+                case "list":
+                    return new ListDataAccess();
+                default:
+                    return new ListDataAccess();
+
+            }
+        }
+    }
+}
